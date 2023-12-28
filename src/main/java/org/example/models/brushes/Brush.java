8978463927f9b4ebfc5120243ed2ead3b0362c11
Denis <denispudelka@gmail.com>
2023-12-28 18:33:54 +0100

    Using Path2D for brush.

--- src/main/java/org/example/models/brushes/Brush.java
+++ src/main/java/org/example/models/brushes/Brush.java
@@ -3,30 +3,30 @@ package org.example.models.brushes;
 import org.example.models.helpClasses.GeometryShape;
 
 import java.awt.*;
+import java.awt.geom.Path2D;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Brush extends GeometryShape {
-    private List<Point> points;
+    private Path2D path2D;
     private int brushSize;
 
     public Brush(int x, int y, int brushSize, Color color) {
         super(x,y,0, color);
-        this.points = new ArrayList<>();
         this.brushSize = brushSize;
-        this.points.add(new Point(x,y));
+        this.path2D = new Path2D.Float();
+        this.path2D.moveTo(x,y);
     }
 
     public void addPoint(int x, int y){
-        points.add(new Point(x,y));
+        this.path2D.lineTo(x,y);
     }
 
     @Override
     public void draw(Graphics2D graphics2D) {
         graphics2D.setColor(getColor());
-        for(Point point : points){
-            graphics2D.fillOval(point.x - brushSize / 2, point.y - brushSize / 2, brushSize, brushSize);
-        }
+        graphics2D.setStroke(new BasicStroke(brushSize, BasicStroke.CAP_ROUND, BasicStroke.JOIN_BEVEL));
+        graphics2D.draw(path2D);
     }
 
     @Override
