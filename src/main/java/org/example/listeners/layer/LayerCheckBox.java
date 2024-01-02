abb251b2bf6d0123b20eaced3c08d288ca4939a5
Denis <denispudelka@gmail.com>
2024-01-02 14:45:01 +0100

    Fixed Visibility of a Layer.

--- src/main/java/org/example/listeners/layer/LayerCheckBox.java
+++ src/main/java/org/example/listeners/layer/LayerCheckBox.java
@@ -2,20 +2,39 @@ package org.example.listeners.layer;
 
 import org.example.gui.MainFrame;
 import org.example.listeners.general.GeneralEventListener;
+import org.example.models.layers.Layer;
 
 import java.awt.event.ActionEvent;
+import java.util.List;
 
 public class LayerCheckBox extends GeneralEventListener {
 
     private MainFrame mainFrame;
+    private int layerId;
 
-    public LayerCheckBox(MainFrame mainFrame) {
+    public LayerCheckBox(MainFrame mainFrame, int layerId) {
         this.mainFrame = mainFrame;
+        this.layerId = layerId;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         super.actionPerformed(e);
+        Layer layer = findLayerById(layerId);
+        if(layer != null && layer.isVisible()){
+            layer.setVisible(false);
+        }else {
+            layer.setVisible(true);
+        }
+        mainFrame.getMainCanvas().repaint();
+    }
 
+    private Layer findLayerById(int id) {
+        for (Layer layer : mainFrame.getMainCanvas().getLayersManager().getLayers()) {
+            if (layer.getId() == id) {
+                return layer;
+            }
+        }
+        return null;
     }
 }
