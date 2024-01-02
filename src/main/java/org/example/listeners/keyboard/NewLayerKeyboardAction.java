48423887aa156defd8bb4c70aaeacabb869299eb
Denis <denispudelka@gmail.com>
2024-01-02 14:25:28 +0100

    Fixing New Layer Button features.

--- src/main/java/org/example/listeners/keyboard/NewLayerKeyboardAction.java
+++ src/main/java/org/example/listeners/keyboard/NewLayerKeyboardAction.java
@@ -1,6 +1,9 @@
 package org.example.listeners.keyboard;
 
 import org.example.gui.MainFrame;
+import org.example.gui.east_panel.LayerPanel;
+import org.example.gui.east_panel.LayersPanel;
+import org.example.models.layers.Layer;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -23,6 +26,15 @@ public class NewLayerKeyboardAction extends AbstractAction {
 */
     @Override
     public void actionPerformed(ActionEvent e) {
+        Layer newLayer = mainFrame.getMainCanvas().getLayersManager().addLayer();
+        mainFrame.getMenuPanel().getSouthPanel().getColorIndicator().setColorSquare(newLayer.getCurrentColor());
+        mainFrame.getMainCanvas().getLayersManager().setActiveLayer(newLayer);
 
+        LayerPanel newLayerPanel = new LayerPanel(mainFrame, newLayer.getId(), newLayer.isVisible());
+
+        LayersPanel layersPanel = mainFrame.getEastPanel().getLayersPanel();
+        layersPanel.getLayersPanel().add(newLayerPanel);
+        layersPanel.getLayersPanel().revalidate();
+        layersPanel.getLayersPanel().repaint();
     }
 }
